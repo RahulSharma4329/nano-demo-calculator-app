4b1cc2c79903d19eac8730e1cb1274eb3fdcd3c1
RahulSharma4329 <rahulsharma4329@gmail.com>
2023-08-02 13:53:30 +0530

    added functionality of endpoints

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@
 #include <iostream>
 int main()
 {
-  crow::SimpleApp app;
-  CROW_ROUTE(app, "/calculator/greeting")
-      .methods("GET"_method)(&greet);
-  CROW_ROUTE(app, "/calculator/add")
-      .methods("POST"_method)(&add);
-  CROW_ROUTE(app, "/calculator/subtract")
-      .methods("POST"_method)(&subtract);
+    crow::SimpleApp app;
+    CROW_ROUTE(app, "/calculator/greeting")
+        .methods("GET"_method)(&greet);
+    CROW_ROUTE(app, "/calculator/add")
+        .methods("POST"_method)(&add);
+    CROW_ROUTE(app, "/calculator/subtract")
+        .methods("POST"_method)(&subtract);
 
-  app.port(8080).run();
-  return 0;
+    app.port(8080).run();
+    return 0;
 }
--- routes.cpp
+++ routes.cpp
@@ -2,15 +2,17 @@
 
 crow::response greet()
 {
-    return crow::response{""};
+    return crow::response{"Hello world!"};
 }
 crow::response add(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    int sum = x["first"].i() + x["second"].i();
+    return crow::response{"result", sum};
 }
 crow::response subtract(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    int diff = x["first"].i() - x["second"].i();
+    return crow::response{"result", diff};
 }
