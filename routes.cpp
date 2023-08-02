3bb5f1d7dceec0efd00c485805f4a5f83e9a84f1
RahulSharma4329 <rahulsharma4329@gmail.com>
2023-08-02 14:03:49 +0530

    fixed errors

--- routes.cpp
+++ routes.cpp
@@ -2,17 +2,19 @@
 
 crow::response greet()
 {
-    return crow::response{"Hello world!"};
+    return crow::response{200, "Hello world!"};
 }
 crow::response add(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
     int sum = input["first"].i() + input["second"].i();
-    return crow::response{"result", sum};
+    json result = {"result", sum};
+    return crow::response{200, result};
 }
 crow::response subtract(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
     int diff = input["first"].i() - input["second"].i();
-    return crow::response{"result", diff};
+    json resp = {"result", diff};
+    return crow::response{200, resp};
 }
